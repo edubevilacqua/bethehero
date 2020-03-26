9f0aa1259002fd4c73f875d428d48cf964c1e4bd
Eduardo Bevilacqua <edu_bevilacqua@outlook.com>
2020-03-25 22:33:11 -0300

    build

---
+++ hero_frontend/build/precache-manifest.fb866b7507467c03f8b43ca77a4faea7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5405a5c8eb17d1ca0218e6952914e7f3",
+    "url": "/bethehero/index.html"
+  },
+  {
+    "revision": "64923a04f38c4828be68",
+    "url": "/bethehero/static/css/main.37117ba0.chunk.css"
+  },
+  {
+    "revision": "de788b618da42dc67498",
+    "url": "/bethehero/static/js/2.51b7343b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bethehero/static/js/2.51b7343b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64923a04f38c4828be68",
+    "url": "/bethehero/static/js/main.f13dc2cd.chunk.js"
+  },
+  {
+    "revision": "a358e7b8f12f439d6fbf",
+    "url": "/bethehero/static/js/runtime-main.38734c10.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/bethehero/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/bethehero/static/media/logo.7eea718b.svg"
+  }
+]);
